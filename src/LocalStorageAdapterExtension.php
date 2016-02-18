b6451b158a62053386df3c80c0ab6b621105e113
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 00:31:29 -0600

    Author

--- src/LocalStorageAdapterExtension.php
+++ src/LocalStorageAdapterExtension.php
@@ -7,9 +7,9 @@ use Anomaly\LocalStorageAdapterExtension\Command\LoadDisk;
 /**
  * Class LocalStorageAdapterExtension
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\LocalStorageAdapterExtension
  */
 class LocalStorageAdapterExtension extends AdapterExtension
