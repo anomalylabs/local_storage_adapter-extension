21d83910bb1d1b9c296084a68956180cfe71870d
Ryan Thompson <ryan@pyrocms.com>
2020-02-28 12:36:53 -0600

    Update LoadDisk.php

--- src/Command/LoadDisk.php
+++ src/Command/LoadDisk.php
@@ -69,7 +69,7 @@ class LoadDisk
             $this->disk,
             new Local($root),
             [
-                'base_url' => $private ? null : asset(trim(str_replace(public_path(), '', $root), '/\\')),
+                'base_url' => $root,
             ]
         );
 
